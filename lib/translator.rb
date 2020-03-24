34bc437584fc93e873a5dcfd070c46d2eb62cb87
Derek Williams <jogamer130@gmail.com>
2020-03-24 03:50:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require "yaml"
 
 def load_library
-  emoticons = YAML.load_file("./lib/emoticons.yml")
+  emoticons = YAML.load_file('./lib/emoticons.yml')
   
   emoticon_mean = {"get_meaning" => {}, "get_emot" => {} }
   
